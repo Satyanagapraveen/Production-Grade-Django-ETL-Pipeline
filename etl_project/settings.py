8aca32aa9cd9d7b5547fcb7ab4c825bf06ce2507
SATYA NAGA PRAVEEN KUMAR NAMBURI <153361064+Satyanagapraveen@users.noreply.github.com>
2026-06-12 14:53:54 +0530

    Update settings.py to configure allowed hosts and switch database to PostgreSQL

--- etl_project/settings.py
+++ etl_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-key-change
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'etl_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'legacy_db',
+        'USER': 'etl_user',
+        'PASSWORD': 'etl_password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
